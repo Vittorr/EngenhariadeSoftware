b55b072a8b961fffed7ba7536976d8e5b07cd110
Vittorr <paulobjalves@hotmail.com>
2022-12-05 19:35:39 -0300

    Finalizando desafio

--- Desafios/desafio1.js
+++ Desafios/desafio1.js
@@ -3,6 +3,8 @@ class Book {
         this.title = title;
         this.description = description;
         this.author = author;
+        // this.id = String(parseInt(Math.random() * 100000))
+        this.id = String(description.length + 3)
     }
 }
 
@@ -11,9 +13,8 @@ class Library {
         this.books = []
     }
 
-    addBook(bookInfo, id) {
-        let book = new Book(bookInfo[0], bookInfo[1], bookInfo[2])
-        book['id'] = id
+    addBook(bookInfo) {
+        let book = new Book(bookInfo['title'], bookInfo['description'], bookInfo['author'])
         this.books.push(book)
 
         return book
@@ -24,42 +25,65 @@ class Library {
     }
 
     removeBookById(id) {
+        let cond = false;
         for (let i = 0; i < this.books.length; i++) {
             if (this.books[i]['id'] === id) {
                 this.books.splice(i, 1);
-
-                return this.books[i]
-            }
+                cond = true;
+                break
+            } 
+        }
+        if (cond === false) {
+            console.log('Livro não encontrado')
         }
     }
 
     getBookById(id) {
+        let cond = false;
         for (let i = 0; i < this.books.length; i++) {
             if (this.books[i]['id'] === id) {
+                cond = true;
                 return this.books[i]
-            }
+            } 
+        }
+        if (cond === false) {
+            console.log('Livro não encontrado')
         }
     }
 
     updateBookById(id, info) {
+        let cond = false;
         for (let i = 0; i < this.books.length; i++) {
             if (this.books[i]['id'] === id) {
-                this.books[i]['title'] = info[0];
-                this.books[i]['description'] = info[1];
-                this.books[i]['author'] = info[2];
-
+                this.books[i]['title'] = info['title'];
+                this.books[i]['description'] = info['description'];
+                this.books[i]['author'] = info['author'];
+                
+                cond = true;
                 return this.books[i]
             }
+        } 
+        if (cond === false) {
+            console.log('Livro não encontrado')
         }
     }
 }
 
 const p1 = new Library()
 
-p1.addBook(['haha', 'olaa', 'vitor'], '123')
-p1.addBook(['haha', 'olaa', 'vitor'], '453')
-p1.addBook(['haha', 'olaa', 'vitor'], '321')
+p1.addBook({title: 'Terra', description: 'Solo', author: 'Vitor'})
+p1.addBook({title: 'Sol', description: 'Chamas', author: 'João'})
+p1.addBook({title: 'Lua', description: 'Noite', author: 'Lucas'})
 
-p1.updateBookById('123', ['O amanhecer', 'Fala sobre o Sol', 'Jonas'])
+console.log(p1.getBooks())
+console.log()
+
+p1.removeBookById('7')
+p1.getBookById('7')
+
+console.log(p1.getBooks())
+console.log()
+
+p1.updateBookById('8', {title: 'Terra', description: 'Solo', author: 'Vitor'})
 
 console.log(p1.getBooks())
